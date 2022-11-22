61618542f4ec786b8d47e9ba997c457d0d18ddb5
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-11-22 12:32:37 +0900

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <p className="description">
-          <iframe style='width: 100%; height: 100%; outline: none;' src='https://taja3client.netlify.app/'>죄송하지만 현재 이 브라우저는 iframe요소를 지원하지 않는 것 같습니다.</iframe>
+          <iframe src='https://taja3client.netlify.app/'></iframe>
         </p>
       </main>
     </div>
