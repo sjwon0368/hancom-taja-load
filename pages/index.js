32c89b4f9b47f239177dc5b8553f48480e98b4fc
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-11-22 10:26:45 +0900

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>한컴타자 다이렉트링크</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="" style='display: none;' />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <iframe style='width: 100%; height: 100%; outline: none;' src='https://taja3client.netlify.app/'>죄송하지만 현재 이 브라우저는 iframe요소를 지원하지 않는 것 같습니다.</iframe>
         </p>
       </main>
 
